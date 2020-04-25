ccb55c4e6edff66467a9a3bfb45f872737bb8673
Rahul <rash235r@gmail.com>
2020-04-25 22:58:48 +0200

    chore: basic project setup

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: path.resolve(__dirname, 'src'),
+                use: ['babel-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        port: process.env.PORT || 3000
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/template.html',
+        })
+    ]
+}
+
