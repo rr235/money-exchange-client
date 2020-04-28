14a821ebc64a5b7e7a1314884114ad2d5a3248b6
Rahul <rash235r@gmail.com>
2020-04-28 17:09:33 +0200

    feat: input field can validate input

--- src/components/atoms/input/Input.jsx
+++ src/components/atoms/input/Input.jsx
@@ -1,14 +1,29 @@
-import React, { useState, FormEvent } from 'react';
+import React, { useState } from 'react';
+import { string, func, oneOf } from 'prop-types';
 import classNames from 'classnames';
 import styles from './input.styles.scss';
 
-const Input = ({ label, id, onChange, className }) => {
+const Input = ({ label, id, onChange, className, type }) => {
   const [input, setInput] = useState('');
 
+  const isValidType = (value, inputType) => {
+    if (inputType === 'number') {
+      const valueRegex = /^\d*(\.\d{0,2})?$/; // limits to 2 decimal points
+      return value && valueRegex.test(value);
+    }
+
+    return true;
+  };
+
   const onChangeHandler = (e) => {
-    setInput(e.currentTarget.value);
-    if (onChange) {
-      onChange(e);
+    const { value } = e.currentTarget;
+
+    // update only if input is valid
+    if (isValidType(value, type)) {
+      setInput(value);
+      if (onChange) {
+        onChange(e);
+      }
     }
   };
 
@@ -18,7 +33,7 @@ const Input = ({ label, id, onChange, className }) => {
         {label}
       </label>
       <input
-        type="number"
+        type={type}
         id={id}
         className={styles.input}
         value={input}
@@ -26,6 +41,19 @@ const Input = ({ label, id, onChange, className }) => {
       />
     </>
   );
+};
+
+Input.propTypes = {
+  label: string.isRequired,
+  id: string.isRequired,
+  onChange: func,
+  className: string,
+  type: oneOf(['text', 'number']).isRequired,
+};
+
+Input.defaultProps = {
+  onChange: null,
+  className: null,
 };
 
 export default Input;
