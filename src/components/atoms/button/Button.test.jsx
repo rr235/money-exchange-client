f827a94017bdf8b1fef8ce0f4742a95225b0dbbf
Rahul <rash235r@gmail.com>
2020-04-28 17:24:47 +0200

    test: added snapshot test for button

--- src/components/atoms/button/Button.test.jsx
+++ src/components/atoms/button/Button.test.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
+import renderer from 'react-test-renderer';
 import { shallow } from 'enzyme';
 import Button from './Button';
 
 describe('Button', () => {
-  it('should show correct text', () => {
-    const component = shallow(<Button>Foobar</Button>);
-    expect(component.text()).toBe('Foobar');
+  it('should render correctly', () => {
+    const tree = renderer
+      .create(<Button type="button">Foobar</Button>)
+      .toJSON();
+    expect(tree).toMatchSnapshot();
   });
 
   it('should show correct text', () => {
-    const Content = () => <span>Foobaz</span>;
+    const component = shallow(<Button>Content</Button>);
+    expect(component.text()).toBe('Content');
+  });
+
+  it('should show correct text', () => {
+    const Content = () => <span>Content</span>;
     const component = shallow(
       <Button>
         <Content />
